513b25a08763056af8fbb544a10a5bd326448f02
Priya-V2 <priyav2.bct@gmail.com>
2024-08-21 16:21:54 +0530

    create test and signup api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    res.status(400).json({ message: "All fields are required" });
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("New user created sucessfully");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { signup };
--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoD is connected"));
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
 app.listen("5000", () => {
   console.log("Server is listening to the port 5000");
