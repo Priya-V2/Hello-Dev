6f8e1271fcc7d067af88b27c2a3f71a23d289818
Priya-V2 <priyav2.bct@gmail.com>
2024-08-27 09:03:54 +0530

    Create sign-in api route and add functionality to sign-in page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -30,4 +32,33 @@ const signup = async (req, res, next) => {
   }
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(404, "All fields are required"));
+  }
+  const validUser = await User.findOne({ email });
+  try {
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid Credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Credentials"));
+    }
+    const { password: passcode, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "2d",
+    });
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin };
--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,10 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const ctaColor = loading ? "bg-neon-green-tint" : "bg-neon-green";
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      setErrorMessage("Please fill out all the fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("api/auth/sign-in", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        return navigate("/");
+      }
+    } catch (error) {
+      console.error("Error during submission:", error.errorMessage);
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <div className=" min-h-screen">
-        <div className="flex flex-col font-roboto text-base text-neutral-600 w-80 sm:w-96 px-8 py-6 md:px-10 md:py-10 mx-auto mt-20 shadow-custom-indigo rounded">
+        <div className="flex flex-col font-roboto text-base text-neutral-600 w-80 sm:w-96 px-8 py-6 md:px-10 md:py-8 mx-auto mt-12 shadow-custom-indigo rounded">
           <Link to="/" className="self-center">
             <img
               src="/images/logo-midnight-indigo.png"
@@ -15,7 +55,12 @@ export default function Signin() {
           <p className="text-sm tracking-widest font-bold self-center mb-4">
             A Developer Guide for beginners
           </p>
-          <form>
+          {errorMessage && (
+            <div className=" text-fail-red border bg-fail-red-tint border-red-300 mb-2 p-2 rounded-sm  font-medium text-center">
+              <span>{errorMessage}</span>
+            </div>
+          )}
+          <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email:</label>
             <br />
             <input
@@ -24,6 +69,7 @@ export default function Signin() {
               name="email"
               placeholder="example@gmail.com"
               className="w-full p-2 mt-1 mb-2 border-2 rounded focus:outline-none focus:border-cool-blue"
+              onChange={handleChange}
             />
             <br />
             <label htmlFor="password">Password:</label>
@@ -34,13 +80,26 @@ export default function Signin() {
               name="password"
               placeholder="Your Password"
               className="w-full p-2 mt-1 mb-6 border-2 rounded focus:outline-none focus:border-cool-blue"
+              onChange={handleChange}
             />
             <br />
             <button
               type="submit"
-              className="font-medium text-midnight-indigo bg-neon-green w-full p-1.5 mb-4 border border-midnight-indigo hover:shadow-custom-indigo rounded"
+              className={`font-medium text-midnight-indigo ${ctaColor} w-full p-1.5 mb-4 border border-midnight-indigo hover:shadow-custom-indigo  rounded`}
+              disabled={loading}
             >
-              Sign In
+              {loading ? (
+                <div className="flex justify-center items-center">
+                  <svg
+                    className="animate-spin h-4 w-4 text-midnight-indigo border-4 border-t-transparent border-midnight-indigo rounded-full justify-items-center"
+                    viewBox="0 0 24 24"
+                    style={{ borderWidth: "3px" }}
+                  ></svg>
+                  <span className="ml-2">Loading...</span>
+                </div>
+              ) : (
+                "Sign In"
+              )}
             </button>
             <button className="flex gap-1 justify-center font-medium w-full p-1.5 mb-4 border border-midnight-indigo hover:shadow-custom-indigo rounded">
               <img
@@ -51,9 +110,9 @@ export default function Signin() {
               <span>Google</span>
             </button>
             <div className="flex gap-1">
-              <p>Already have an account?</p>
+              <span>Don&apos;t have an account?</span>
               <Link to={"/sign-up"}>
-                <span className="text-cool-blue">Sign up</span>
+                <span className="text-cool-blue">Sign Up</span>
               </Link>
             </div>
           </form>
