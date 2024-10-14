f800bef617605e080800a230d003cdad92844ac9
Priya-V2 <priyav2.bct@gmail.com>
2024-10-14 11:25:38 +0530

    create get users api route

--- api/controllers/user.contoller.js
+++ api/controllers/user.contoller.js
@@ -2,8 +2,51 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
+const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    next(errorHandler(403, "You're not allowed to see all the users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const totalUsers = await User.countDocuments();
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You're not allowed to update the user"));
   }
   if (req.body.password) {
@@ -52,7 +95,7 @@ const updateUser = async (req, res, next) => {
 };
 
 const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
@@ -74,4 +117,4 @@ const signoutUser = (req, res, next) => {
   }
 };
 
-export { updateUser, deleteUser, signoutUser };
+export { getUsers, updateUser, deleteUser, signoutUser };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUsers,
   signoutUser,
   updateUser,
 } from "../controllers/user.contoller.js";
@@ -8,6 +9,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
+router.get("/get-users/:userId", verifyToken, getUsers);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signoutUser);
