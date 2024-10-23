e0568657a47bb18d7868b2cfb8fa55162edaf534
Priya-V2 <priyav2.bct@gmail.com>
2024-10-23 20:03:03 +0530

    create edit comment api route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,6 +3,7 @@ import {
   createComment,
   getComment,
   likeComment,
+  editComment,
 } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/get-comment/:postId", getComment);
 router.put("/like-comment/:commentId", verifyToken, likeComment);
+router.put("/edit-comment/:commentId", verifyToken, editComment);
 
 export default router;
