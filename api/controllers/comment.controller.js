5ea552bd6d3aeecf1f0a768b91a29642c791384a
Priya-V2 <priyav2.bct@gmail.com>
2024-10-26 13:50:00 +0530

    create get comments api route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -35,6 +35,33 @@ const getComment = async (req, res, next) => {
   }
 };
 
+const getComments = async (req, res, next) => {
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not allowed to get all comments"));
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "desc" ? -1 : 1;
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const likeComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
@@ -98,4 +125,4 @@ const deleteComment = async (req, res, next) => {
   }
 };
 
-export { createComment, getComment, likeComment, deleteComment };
+export { createComment, getComment, getComments, likeComment, deleteComment };
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createComment,
   getComment,
+  getComments,
   likeComment,
   editComment,
   deleteComment,
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/get-comment/:postId", getComment);
+router.get("/get-comments", verifyToken, getComments);
 router.put("/like-comment/:commentId", verifyToken, likeComment);
 router.put("/edit-comment/:commentId", verifyToken, editComment);
 router.delete("/delete-comment/:commentId", verifyToken, deleteComment);
