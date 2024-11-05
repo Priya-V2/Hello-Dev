20bfc1b67ce8e98ae83715e6b2f259fedd26557c
Priya-V2 <priyav2.bct@gmail.com>
2024-11-05 11:42:41 +0530

    edit tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
@@ -26,4 +28,5 @@ export const theme = {
     },
   },
 };
-export const plugins = [require("tailwind-scrollbar")];
+
+export const plugins = [tailwindScrollbar];
