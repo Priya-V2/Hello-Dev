8c6a699aa0590df761cda9221b14544ee64b497a
Priya-V2 <priyav2.bct@gmail.com>
2024-11-11 23:41:25 +0530

    changing the url's in api to debug the google signin error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -102,7 +102,7 @@ const googleCallback = (req, res) => {
     res.redirect(
       process.env.NODE_ENV === "production"
         ? process.env.GOOGLE_CALLBACK_URL
-        : "http://localhost:5000/api/auth/google/callback"
+        : "http://localhost:3000/api/auth/google/callback"
     );
   })(req, res);
 };
