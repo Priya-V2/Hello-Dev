cfd100727da0cb845593111385a86ee55eb772d4
Priya-V2 <priyav2.bct@gmail.com>
2024-11-12 10:27:23 +0530

    edit redirect url

--- api/index.js
+++ api/index.js
@@ -30,16 +30,16 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-// app.use(
-//   cors({
-//     origin:
-//       process.env.NODE_ENV === "production"
-//         ? "https://hello-dev.onrender.com"
-//         : "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://hello-dev.onrender.com"
+        : "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
