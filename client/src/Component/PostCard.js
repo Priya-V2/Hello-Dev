e654f0fbb316b286e380c8186b3586eaea3165a7
Priya-V2 <priyav2.bct@gmail.com>
2025-02-09 23:28:32 +0530

    edit ui of post card for improved design

--- client/src/Component/PostCard.js
+++ client/src/Component/PostCard.js
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 
-export default function PostCard({ relatedPost }) {
+export default function PostCard({ relatedPost: post }) {
   return (
     <div className="font-roboto max-w-72 w-full mx-auto my-4">
-      <div className="flex justify-center">
-        <img
-          className="w-full max-w-72 max-h-36 sm:max-w-56 sm:max-h-28 lg:max-w-72 lg:max-h-36 mb-2 sm:mb-4 object-cover rounded transform transition-transform duration-300 ease-in-out hover:scale-110"
-          src={relatedPost && relatedPost.image}
-          alt={relatedPost && relatedPost.title}
-        />
-      </div>
-      <Link to={`/post/${relatedPost.slug}`}>
-        <p className="font-medium text-base sm:text-lg text-center text-dark-charcoal hover:text-blue-700 hover:underline hover:cursor-pointer">
-          {relatedPost && relatedPost.title}
+      <Link to={`/post/${post.slug}`}>
+        <div className="relative flex justify-center overflow-hidden bg-cover bg-no-repeat">
+          <img
+            className="w-full object-fill rounded "
+            src={post && post.image}
+            alt={post && post.title}
+          />
+          <div class="absolute h-full w-full overflow-hidden bg-neutral-500 rounded bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-40"></div>
+        </div>
+        <p className="font-medium text-base lg:text-lg text-left text-dark-charcoal mt-2 sm:mt-4 hover:text-blue-700 hover:underline hover:cursor-pointer">
+          {post && post.title}
         </p>
       </Link>
     </div>
