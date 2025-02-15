d585939c25d9207c6a1ab9009eb6830f0c0da644
Priya-V2 <priyav2.bct@gmail.com>
2025-02-15 12:38:06 +0530

    add bookmarks field to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema(
       type: Date,
       default: null,
     },
+    bookmarks: {
+      type: Array,
+      default: [],
+    },
   },
   { timestamps: true }
 );
